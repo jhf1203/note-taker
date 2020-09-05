33edb6cda46a90c8f0bd3a42d37d544462caa7ac
Jim Faulkner <jhf1203@gmail.com>
2020-09-05 10:38:07 -0400

    altering code for heroku functionality

--- develop/db/server.js
+++ develop/db/server.js
@@ -19,7 +19,7 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-app.get("/index", function(req, res) {
+app.get("/", function(req, res) {
      res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
